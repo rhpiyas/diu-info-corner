48e5ce9d1d02270dddfe4a819b3cd813d3a2cacc
Rakib Hasan Piyas <rakibhasanpiyas97@gmail.com>
2026-07-03 17:29:02 +0600

    Add Admin login info

--- src/main.c
+++ src/main.c
@@ -18,6 +18,15 @@ struct User
     char pin[5];
 };
 
+struct User admins[5] =
+    {
+        {"01632725621", "piyas242-35-182@diu.edu.bd", "4466"},
+        {"01601793978", "monira242-35-637@diu.edu.bd", "2107"},
+        {"01315-500511", "ahmed242-35-580@diu.edu.bd", "7135"},
+        {"01303072338", "zihad242-35-435@diu.edu.bd", "4411"},
+        {"01853828261", "atikur242-35-632@diu.edu.bd", "2481"}
+    };
+
 int main()
 {
     
